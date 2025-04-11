5b37cb105eaa29332711e0dc3b251acd79ddccf6
AidanPC <acd7736@uncw.edu>
2025-04-11 11:14:58 -0400

    Arduino IO reporting data to webserver. Project code complete for now.

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,64 @@
 #include <Arduino.h>
 #include <DHT.h>
-#include <ESP8266WiFi.h>
 #include "secrets.h"
+#include <AdafruitIO.h>
 
 #define DHT_PIN 5
 #define DHT_TYPE DHT11
 
-const char* ssid = WIFI_SSID;
-const char* password = WIFI_PASSWORD;
-
 DHT dht = DHT(DHT_PIN, DHT_TYPE);
 
-float t = 0.0;
-float h = 0.0;
+// io and wifi connection defined in Adafruit IO config file. See example config.h under Adafruit IO library examples for setup.
+AdafruitIO_Feed *temperature = io.feed("temperature_feed");
+AdafruitIO_Feed *humidity = io.feed("humidity_feed");
+
+float dht_temperature_celcius = 0.0;
+float dht_temperature_fahrenheit = 0.0;
+float dht_humidity = 0.0;
 
 void setup() {
   Serial.begin(9600);
 
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi...");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
+  dht.begin();
+  
+  // WiFi now handled by Adafruit IO
+  Serial.print("Connecting to Adafruit IO");
+  io.connect();
+
+  while(io.status() < AIO_CONNECTED) {
     Serial.print(".");
+    delay(500);
   }
 
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  
-  dht.begin();
+  Serial.println();
+  Serial.println(io.statusText());
 }
 
+const int send_interval = 15 * 60 * 1000; // Send once every 15 minutes; 15 Minutes * 60 Seconds * 1000 Milliseconds
+unsigned long last_send_time = 0;
+
 void loop() {
-  t = dht.readTemperature();
-  t = dht.convertCtoF(t);
-  h = dht.readHumidity();
+  io.run();
 
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.println("%");
+  unsigned long current_time = millis();
 
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.println(" *F");
+  if (current_time - last_send_time >= send_interval) {
+    dht_temperature_celcius = dht.readTemperature();
+    dht_temperature_fahrenheit = dht.convertCtoF(dht_temperature_celcius);
+    dht_humidity = dht.readHumidity();
 
-  delay(1000);
+    temperature->save(dht_humidity);
+    Serial.print("Humidity: ");
+    Serial.print(dht_humidity);
+    Serial.println("%");
+
+    humidity->save(dht_humidity);
+    Serial.print("Temperature: ");
+    Serial.print(dht_temperature_fahrenheit);
+    Serial.println(" *F");
+
+    last_send_time = current_time;
+  } 
+
+  delay(5000);
 }
