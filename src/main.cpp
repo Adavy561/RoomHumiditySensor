647f256652992faaafc5803fbd143cdfbcc56b10
AidanPC <acd7736@uncw.edu>
2025-04-11 10:13:19 -0400

    WiFi connected

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <Arduino.h>
 #include <DHT.h>
+#include <ESP8266WiFi.h>
+#include "secrets.h"
 
 #define DHT_PIN 5
 #define DHT_TYPE DHT11
+
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASSWORD;
 
 DHT dht = DHT(DHT_PIN, DHT_TYPE);
 
@@ -11,6 +16,18 @@ float h = 0.0;
 
 void setup() {
   Serial.begin(9600);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  
   dht.begin();
 }
 
@@ -25,7 +42,7 @@ void loop() {
 
   Serial.print("Temperature: ");
   Serial.print(t);
-  Serial.println(" F");
+  Serial.println(" *F");
 
   delay(1000);
 }
